a03c4792abd8eb12c21a4093665abb3807b68c22
albingcj <114584909+albingcj@users.noreply.github.com>
2023-08-24 15:58:04 +0530

    Update config.php
    Check if connection failed with db

--- includes/config.php
+++ includes/config.php
@@ -4,4 +4,8 @@ define('DB_USERNAME','root');
 define('DB_PASSWORD','');
 define('DB_DATABASE','five');
 $db=mysqli_connect(DB_SERVER,DB_USERNAME,DB_PASSWORD,DB_DATABASE);
+// Check connection
+if (!$db) {
+    die("Connection failed: " . mysqli_connect_error());
+}
 ?>
