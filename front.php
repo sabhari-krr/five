4197674b9da82a94dcfc70185874f791a966ef26
albingcj <114584909+albingcj@users.noreply.github.com>
2023-08-24 22:49:14 +0530

    Update front.php

--- front.php
+++ front.php
@@ -23,25 +23,25 @@ if ($results === false) {
             <div class="waste d-block d-sm-none" style="height: 54px;">adsasdasd</div>
             <div class="col justify-content-center">
                 <!-- feed -->
-                <div class="row bg-secondary justify-content-center">
+                <div class="row bg-light justify-content-center">
                     <!-- Content for the first row -->
 
 
                     <div class="col-auto justify-content-center g-5">
                         <?php foreach ($articles as $article) : ?>
-                            <div class="col-12 mb-1">
-                                <div class="card col">
-                                    <div class="col ms-2 fs-5 fw-bold">
-                                        <a href="article.php?id=<?= $article['id']; ?>">
+                            <div class="col-12 mb-1 p-2">
+                                <div class="card col shadow ">
+                                    <div class="col fs-5 pt-2 ps-3 fw-bold pb-2 bg-gradient bg-warning rounded-top">
+                                        <a href="article.php?id=<?= $article['id']; ?>" class="text-light text-decoration-none">
                                             <?= htmlspecialchars($article['username']); ?>
                                         </a>
                                     </div>
-                                    <div class="col ms-2 fs-6">
+                                    <div class="col p-2 pb-2 fs-6">
                                         <?= htmlspecialchars($article['caption']); ?>
                                     </div>
                                     <div class="col">
                                         <div class="justify-content-center">
-                                            <img class="img-fluid" src="<?= htmlspecialchars($article['img']); ?>" alt="" width="600" height="300" />
+                                            <img class="img-fluid rounded-bottom" src="<?= htmlspecialchars($article['img']); ?>" alt="" width="600" height="300" />
                                         </div>
                                     </div>
                                 </div>
@@ -51,14 +51,14 @@ if ($results === false) {
                 </div>
             </div>
 
-            <div class="col-4 col-lg-3 bg-light sticky-top d-none d-sm-block" style="top: 58px; height: 95vh">
-                <div class="container mt-5">
+            <div class="col-4 col-lg-3 bg-light sticky-top d-none d-lg-block shadow " style="top: 58px; height: 95vh">
+                <div class="container mt-5 ">
                     <div class="row justify-content-center">
-                        <div class="col text-center p-3">
+                        <div class="col text-center p-5">
                             <div class="rounded-circle overflow-hidden mx-auto shadow-lg" style="
                     display: flex;
                     justify-content: center;
-                    align-items: center;
+                      align-items: center;
                     ">
                                 <img class="img-fluid" src="https://via.placeholder.com/200" alt="Circular Image" style="width: 100%; height: 100%; object-fit: cover" />
                             </div>
