c20b8f2f162157a77237bf8a2ff758bc95733d84
albingcj <114584909+albingcj@users.noreply.github.com>
2023-08-26 00:46:16 +0530

    minor changes

--- profile.php
+++ profile.php
@@ -21,7 +21,7 @@ if ($_SESSION['username']) {
 
 
 
-//selecting all the posts according to newwest 
+  //selecting all the posts according to newwest 
 
   $sql2 = "SELECT *
         FROM content WHERE username='$usercnfrm'
@@ -39,316 +39,310 @@ if ($_SESSION['username']) {
 
 <?= include "includes/head.php"; ?>
 
-  <style>
+<style>
+  /* General Scrollbar Styling */
+  ::-webkit-scrollbar {
+    width: 10px;
+    /* Width of the scrollbar */
+  }
+
+  /* Track */
+  ::-webkit-scrollbar-track {
+    background: #f1f1f1;
+    /* Color of the track (background) */
+    border-radius: 10px;
+    /* Rounded corners */
+  }
+
+  /* Handle (Thumb) */
+  ::-webkit-scrollbar-thumb {
+    background: #888;
+    /* Color of the handle */
+    border-radius: 10px;
+    /* Rounded corners */
+  }
+
+  /* Handle (Thumb) on hover */
+  ::-webkit-scrollbar-thumb:hover {
+    background: #555;
+    /* Darken the handle on hover */
+  }
+
+  body {
+    width: 100%;
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+    min-height: 100vh;
+    font-family: "Poppins", sans-serif;
+    background-color: lightblue;
+  }
 
 
-    /* General Scrollbar Styling */
-    ::-webkit-scrollbar {
-      width: 10px;
-      /* Width of the scrollbar */
-    }
+  ul {
+    list-style-type: none;
+    margin: 0;
+    padding: 0;
+    display: flex;
+    align-items: center;
+  }
 
-    /* Track */
-    ::-webkit-scrollbar-track {
-      background: #f1f1f1;
-      /* Color of the track (background) */
-      border-radius: 10px;
-      /* Rounded corners */
-    }
+  a {
+    text-decoration: none;
+  }
 
-    /* Handle (Thumb) */
-    ::-webkit-scrollbar-thumb {
-      background: #888;
-      /* Color of the handle */
-      border-radius: 10px;
-      /* Rounded corners */
-    }
+  .header__wrapper header {
+    width: 100%;
+    background: url("bg.jpeg") no-repeat 50% 20% / cover;
+    min-height: calc(10px + 15vw);
+  }
 
-    /* Handle (Thumb) on hover */
-    ::-webkit-scrollbar-thumb:hover {
-      background: #555;
-      /* Darken the handle on hover */
-    }
+  .header__wrapper .cols__container .left__col {
+    padding: 25px 20px;
+    text-align: center;
+    max-width: 350px;
+    position: relative;
+    margin: 0 auto;
+  }
 
-    body {
-      width: 100%;
-      margin: 0;
-      padding: 0;
-      box-sizing: border-box;
-      min-height: 100vh;
-      font-family: "Poppins", sans-serif;
-      background-color: lightblue;
-    }
+  .header__wrapper .cols__container .left__col .img__container {
+    position: absolute;
+    top: -60px;
+    left: 50%;
+    transform: translatex(-50%);
+  }
 
+  .header__wrapper .cols__container .left__col .img__container img {
+    width: 120px;
+    height: 120px;
+    object-fit: cover;
+    border-radius: 50%;
+    display: block;
+    box-shadow: 1px 3px 12px rgba(0, 0, 0, 0.18);
+  }
 
-    ul {
-      list-style-type: none;
-      margin: 0;
-      padding: 0;
-      display: flex;
-      align-items: center;
-    }
+  .header__wrapper .cols__container .left__col .img__container span {
+    position: absolute;
+    background: #2afa6a;
+    width: 16px;
+    height: 16px;
+    border-radius: 50%;
+    bottom: 3px;
+    right: 11px;
+    border: 2px solid #fff;
+  }
 
-    a {
-      text-decoration: none;
-    }
+  .header__wrapper .cols__container .left__col h2 {
+    margin-top: 60px;
+    font-weight: 600;
+    font-size: 22px;
+    margin-bottom: 5px;
+  }
 
-    .header__wrapper header {
-      width: 100%;
-      background: url("bg.jpeg") no-repeat 50% 20% / cover;
-      min-height: calc(10px + 15vw);
+  .header__wrapper .cols__container .left__col p {
+    font-size: 0.9rem;
+    color: #818181;
+    margin: 0;
+  }
+
+  .header__wrapper .cols__container .left__col .about {
+    justify-content: space-between;
+    position: relative;
+    margin: 35px 0;
+  }
+
+  .header__wrapper .cols__container .left__col .about li {
+    display: flex;
+    flex-direction: column;
+    color: #818181;
+    font-size: 0.9rem;
+  }
+
+  .header__wrapper .cols__container .left__col .about li span {
+    color: #1d1d1d;
+    font-weight: 600;
+  }
+
+  .header__wrapper .cols__container .left__col .about:after {
+    position: absolute;
+    content: "";
+    bottom: -16px;
+    display: block;
+    background: #cccccc;
+    height: 1px;
+    width: 100%;
+  }
+
+  .header__wrapper .cols__container .content p {
+    font-size: 1rem;
+    color: #1d1d1d;
+    line-height: 1.8em;
+  }
+
+  .header__wrapper .cols__container .content ul {
+    gap: 30px;
+    justify-content: center;
+    align-items: center;
+    margin-top: 25px;
+  }
+
+  .header__wrapper .cols__container .content ul li {
+    display: flex;
+  }
+
+  .header__wrapper .cols__container .content ul i {
+    font-size: 1.3rem;
+  }
+
+  .header__wrapper .cols__container .right__col nav {
+    display: flex;
+    align-items: center;
+    padding: 30px 0;
+    justify-content: space-between;
+    flex-direction: column;
+  }
+
+  .header__wrapper .cols__container .right__col nav ul {
+    display: flex;
+    gap: 20px;
+    flex-direction: column;
+  }
+
+  .header__wrapper .cols__container .right__col nav ul li a {
+    text-transform: uppercase;
+    color: #818181;
+  }
+
+  .header__wrapper .cols__container .right__col nav ul li:nth-child(1) a {
+    color: #1d1d1d;
+    font-weight: 600;
+  }
+
+  .header__wrapper .cols__container .right__col nav button {
+    background: lightslategray;
+    color: #fff;
+    border: none;
+    padding: 10px 25px;
+    border-radius: 4px;
+    cursor: pointer;
+    margin-top: 20px;
+  }
+
+  .header__wrapper .cols__container .right__col nav button:hover {
+    opacity: 0.8;
+  }
+
+  .header__wrapper .cols__container .right__col .photos  {
+    display: grid;
+    grid-template-columns: repeat(auto-fill, minmax(190px, 1fr));
+    gap: 20px;
+    height: 365px;
+    overflow: auto;
+    padding: 0 0 30px;
+  }
+
+  .header__wrapper .cols__container .right__col .photos img {
+    max-width: 100%;
+    display: block;
+    height: 100%;
+    object-fit: cover;
+  }
+
+  @media (min-width: 868px) {
+    .header__wrapper .cols__container {
+      max-width: 1200px;
+      margin: 0 auto;
+      width: 90%;
+      justify-content: space-between;
+      display: grid;
+      grid-template-columns: 1fr 2fr;
+      gap: 50px;
     }
 
     .header__wrapper .cols__container .left__col {
-      padding: 25px 20px;
-      text-align: center;
-      max-width: 350px;
-      position: relative;
-      margin: 0 auto;
-    }
-
-    .header__wrapper .cols__container .left__col .img__container {
-      position: absolute;
-      top: -60px;
-      left: 50%;
-      transform: translatex(-50%);
-    }
-
-    .header__wrapper .cols__container .left__col .img__container img {
-      width: 120px;
-      height: 120px;
-      object-fit: cover;
-      border-radius: 50%;
-      display: block;
-      box-shadow: 1px 3px 12px rgba(0, 0, 0, 0.18);
-    }
-
-    .header__wrapper .cols__container .left__col .img__container span {
-      position: absolute;
-      background: #2afa6a;
-      width: 16px;
-      height: 16px;
-      border-radius: 50%;
-      bottom: 3px;
-      right: 11px;
-      border: 2px solid #fff;
-    }
-
-    .header__wrapper .cols__container .left__col h2 {
-      margin-top: 60px;
-      font-weight: 600;
-      font-size: 22px;
-      margin-bottom: 5px;
-    }
-
-    .header__wrapper .cols__container .left__col p {
-      font-size: 0.9rem;
-      color: #818181;
-      margin: 0;
-    }
-
-    .header__wrapper .cols__container .left__col .about {
-      justify-content: space-between;
-      position: relative;
-      margin: 35px 0;
-    }
-
-    .header__wrapper .cols__container .left__col .about li {
-      display: flex;
-      flex-direction: column;
-      color: #818181;
-      font-size: 0.9rem;
-    }
-
-    .header__wrapper .cols__container .left__col .about li span {
-      color: #1d1d1d;
-      font-weight: 600;
-    }
-
-    .header__wrapper .cols__container .left__col .about:after {
-      position: absolute;
-      content: "";
-      bottom: -16px;
-      display: block;
-      background: #cccccc;
-      height: 1px;
-      width: 100%;
-    }
-
-    .header__wrapper .cols__container .content p {
-      font-size: 1rem;
-      color: #1d1d1d;
-      line-height: 1.8em;
-    }
-
-    .header__wrapper .cols__container .content ul {
-      gap: 30px;
-      justify-content: center;
-      align-items: center;
-      margin-top: 25px;
-    }
-
-    .header__wrapper .cols__container .content ul li {
-      display: flex;
-    }
-
-    .header__wrapper .cols__container .content ul i {
-      font-size: 1.3rem;
-    }
-
-    .header__wrapper .cols__container .right__col nav {
-      display: flex;
-      align-items: center;
-      padding: 30px 0;
-      justify-content: space-between;
-      flex-direction: column;
+      padding: 25px 0px;
     }
 
     .header__wrapper .cols__container .right__col nav ul {
-      display: flex;
-      gap: 20px;
-      flex-direction: column;
+      flex-direction: row;
+      gap: 30px;
     }
 
-    .header__wrapper .cols__container .right__col nav ul li a {
-      text-transform: uppercase;
-      color: #818181;
+  @media (min-width: 1017px) {
+    .header__wrapper .cols__container .left__col {
+      margin: 0;
+      margin-right: auto;
     }
 
-    .header__wrapper .cols__container .right__col nav ul li:nth-child(1) a {
-      color: #1d1d1d;
-      font-weight: 600;
+    .header__wrapper .cols__container .right__col nav {
+      flex-direction: row;
     }
 
     .header__wrapper .cols__container .right__col nav button {
-      background: lightslategray;
-      color: #fff;
-      border: none;
-      padding: 10px 25px;
-      border-radius: 4px;
-      cursor: pointer;
-      margin-top: 20px;
+      margin-top: 0;
     }
+  }
+</style>
+<div class="header__wrapper">
+  <header></header>
+  <div class="cols__container">
+    <div class="left__col">
 
-    .header__wrapper .cols__container .right__col nav button:hover {
-      opacity: 0.8;
-    }
-
-    .header__wrapper .cols__container .right__col .photos {
-      display: grid;
-      grid-template-columns: repeat(auto-fill, minmax(190px, 1fr));
-      gap: 20px;
-    }
-
-    .header__wrapper .cols__container .right__col .photos img {
-      max-width: 100%;
-      display: block;
-      height: 100%;
-      object-fit: cover;
-    }
-
-    @media (min-width: 868px) {
-      .header__wrapper .cols__container {
-        max-width: 1200px;
-        margin: 0 auto;
-        width: 90%;
-        justify-content: space-between;
-        display: grid;
-        grid-template-columns: 1fr 2fr;
-        gap: 50px;
-      }
-
-      .header__wrapper .cols__container .left__col {
-        padding: 25px 0px;
-      }
-
-      .header__wrapper .cols__container .right__col nav ul {
-        flex-direction: row;
-        gap: 30px;
-      }
-
-      .header__wrapper .cols__container .right__col .photos {
-        height: 365px;
-        overflow: auto;
-        padding: 0 0 30px;
-      }
-    }
-
-    @media (min-width: 1017px) {
-      .header__wrapper .cols__container .left__col {
-        margin: 0;
-        margin-right: auto;
-      }
-
-      .header__wrapper .cols__container .right__col nav {
-        flex-direction: row;
-      }
-
-      .header__wrapper .cols__container .right__col nav button {
-        margin-top: 0;
-      }
-    }
-  </style>
-  <div class="header__wrapper">
-    <header></header>
-    <div class="cols__container">
-      <div class="left__col">
-
-        <div class="img__container">
-          <img src="<?= $img ?>" alt="image" />
-          <span></span>
-        </div>
-
-
-        <h2><?= $name ?></h2>
-        <p>@<?= $uname ?></p>
-        <p><?= $email ?></p>
-        <p></p>
-
-        <ul class="about">
-          <li><span>8,073</span>Followers</li>
-          <li><span>322</span>Following</li>
-          <li><span>4</span>Post</li>
-        </ul>
-
-        <div class="content">
-          <p>
-            <?= $bio ?>
-
-          </p>
-          <br>
-          <br>
-          <form action="" mrthod="Post">
-            <button type="button" class="btn btn-primary">Edit</button>
-            <form>
-              <ul>
-                <li><i class="fab fa-twitter"></i></li>
-                <i class="fab fa-pinterest"></i>
-                <i class="fab fa-facebook"></i>
-                <i class="fab fa-dribbble"></i>
-              </ul>
-        </div>
-
+      <div class="img__container">
+        <img src="<?= $img ?>" alt="image" />
+        <span></span>
       </div>
-      <div class="right__col">
-        <nav>
-          <ul>
-            <li><a href="">photos</a></li>
-            <li><a href="">galleries</a></li>
-            <li><a href="">groups</a></li>
-            <li><a href="">about</a></li>
-          </ul>
-          <button>Follow</button>
-        </nav>
 
-        <div class="photos">
+
+      <h2><?= $name ?></h2>
+      <p>@<?= $uname ?></p>
+      <p><?= $email ?></p>
+      <p></p>
+
+      <ul class="about">
+        <li><span>8,073</span>Followers</li>
+        <li><span>322</span>Following</li>
+        <li><span>4</span>Post</li>
+      </ul>
+
+      <div class="content">
+        <p>
+          <?= $bio ?>
+
+        </p>
+        <br>
+        <br>
+        <form action="" mrthod="Post">
+          <button type="button" class="btn btn-primary">Edit</button>
+          <form>
+            <ul>
+              <li><i class="fab fa-twitter"></i></li>
+              <i class="fab fa-pinterest"></i>
+              <i class="fab fa-facebook"></i>
+              <i class="fab fa-dribbble"></i>
+            </ul>
+      </div>
+
+    </div>
+    <div class="right__col">
+      <nav>
+        <ul>
+          <li><a href="">photos</a></li>
+          <li><a href="">galleries</a></li>
+          <li><a href="">groups</a></li>
+          <li><a href="">about</a></li>
+        </ul>
+        <button>Follow</button>
+      </nav>
+
+      <div class="photos">
         <?php foreach ($articles as $article) : ?>
           <img class="rounded" src="<?= htmlspecialchars($article['img']); ?>" alt="Photo" />
         <?php endforeach; ?>
-        </div>
       </div>
     </div>
   </div>
+</div>
 </body>
 
 </html>
