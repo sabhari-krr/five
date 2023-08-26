587b029550f15953bee2954c7968f66bd45f0ea1
albingcj <114584909+albingcj@users.noreply.github.com>
2023-08-26 15:05:19 +0530

    username

--- profile.php
+++ profile.php
@@ -4,7 +4,7 @@
 <?php
 
 include_once "includes/config.php";
-$_SESSION['username'] = 'lol';
+$_SESSION['username'] = 'narutouzumaki';
 if ($_SESSION['username']) {
   $usercnfrm = $_SESSION['username'];
   $sql = "SELECT * from users where username='$usercnfrm';";
